94ab8272c1c0585b696fa69ffa74c3d210c4cef7
BuildTools <unconfigured@null.spigotmc.org>
2020-03-22 22:13:11 +0100

    edited from remote machine

--- agent_doug.py
+++ agent_doug.py
@@ -1,3 +1,27 @@
+from discord.ext import commands
 
-if __name__ == '__main__':
-    print("hello")
+TOKEN = ''
+
+bot = commands.Bot(command_prefix='!', description='General Purpose Bot')
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def hello(ctx):
+    """Says world"""
+    await ctx.send("world")
+
+
+@bot.command()
+async def add(ctx, left : int, right : int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(TOKEN)

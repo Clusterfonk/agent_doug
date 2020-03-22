5435b01dc00d932e4272dcfdc9b043594d30fe0c
BuildTools <unconfigured@null.spigotmc.org>
2020-03-22 23:57:32 +0100

    edited from remote machine

--- agent_doug.py
+++ agent_doug.py
@@ -1,27 +1,37 @@
 from discord.ext import commands
+import sys
 
-TOKEN = ''
-
-bot = commands.Bot(command_prefix='!', description='General Purpose Bot')
+__BOT = commands.Bot(command_prefix='!', description='General Purpose Bot')
 
 
-@bot.event
+@__BOT.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(__BOT.user.name)
+    print(__BOT.user.id)
     print('------')
 
 
-@bot.command()
+@__BOT.command()
 async def hello(ctx):
     """Says world"""
     await ctx.send("world")
 
 
-@bot.command()
-async def add(ctx, left : int, right : int):
+@__BOT.command()
+async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
-bot.run(TOKEN)
+
+def load_token():
+    try:
+        with open("/.config/discord_bot_token") as token_file:
+            return token_file.read()
+    except FileNotFoundError as error:
+        sys.exit(error.errno)
+
+
+if __name__ == '__main__':
+    token = load_token()
+    __BOT.run(token)

24171f53d9589772eaa47e5c85274eca7e8cc696
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 02:13:16 +0100

    Rephrased Logging Msg

--- agent_doug.py
+++ agent_doug.py
@@ -14,7 +14,8 @@ def set_up_logger():
     logging.basicConfig(
         handlers=[RotatingFileHandler(filename=os.path.expanduser('~/.logs/agent_doug.log'), maxBytes=2000, backupCount=10)],
         format="[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.INFO
     )
 
 

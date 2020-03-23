62b5e8805d7004412a65823b4c76485bb98a22f6
BuildTools <unconfigured@null.spigotmc.org>
2020-03-23 01:27:15 +0100

    refactoring

--- agent_doug.py
+++ agent_doug.py
@@ -30,7 +30,7 @@ async def add(ctx, left: int, right: int):
 def set_up_logger():
     logging.getLogger(__name__)
     logging.basicConfig(
-        handlers=[RotatingFileHandler('agent_doug.log', maxBytes=2000, backupCount=10)],
+        handlers=[RotatingFileHandler('~/logs/agent_doug.log', maxBytes=2000, backupCount=10)],
         format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
         datefmt='%Y-%m-%d %H:%M:%S'
     )

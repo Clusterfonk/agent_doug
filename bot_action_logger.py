6c98add257703dfa1023a3018c527689df5da871
BuildTools <unconfigured@null.spigotmc.org>
2020-03-24 23:15:18 +0100

    removed 2000 byte logs restriction

--- bot_action_logger.py
+++ bot_action_logger.py
@@ -7,7 +7,6 @@ def set_up_logger():
     logging.getLogger(__name__)
     logging.basicConfig(
         handlers=[RotatingFileHandler(filename=os.path.expanduser('~/.logs/agent_doug.log'),
-                                      maxBytes=2000,
                                       backupCount=10)
                   ],
         format="[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s:%(lineno)d] %(message)s",

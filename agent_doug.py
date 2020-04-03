f845cc15ef74bea140d18a880d8724dd62503ee5
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 16:21:13 +0200

    added info cog

--- agent_doug.py
+++ agent_doug.py
@@ -6,6 +6,7 @@ import os
 
 from bot_action_logger import set_up_logger
 from acquire_token import receive_token
+from cogs.info import Info
 from config_parser import BotConfigParser
 
 
@@ -18,17 +19,20 @@ class AgentDoug(commands.Bot):
     def __init__(self, bot_config_parser, **options):
         self.__configure_standard_parameters(bot_config_parser)
         super().__init__(bot_config_parser.get_prefix(), **options)
+        self.__add_cogs()
 
     def __configure_standard_parameters(self, bot_config_parser):
-        self.__bot_name = bot_config_parser.get_bot_name()
-        self.__bot_icon_url = bot_config_parser.get_bot_icon_url()
+        self.bot_name = bot_config_parser.get_bot_name()
+        self.bot_icon_url = bot_config_parser.get_bot_icon_url()
         self.__logging_channel = bot_config_parser.get_log_channel()
         self.__default_role = bot_config_parser.get_default_role()
-        self.__help_description = bot_config_parser.get_help_description()
+        self.help_description = bot_config_parser.get_help_description()
+
+    def __add_cogs(self):
+        self.add_cog(Info)
 
     async def on_ready(self):
-        logging.info('Started {}.'.format(self.__bot_name))
-        print(self.cogs.get_commands(), self.cogs.qualified_name)
+        logging.info('Started {}.'.format(self.bot_name))
 
     async def assign_default_role(self, member):
         if "@everyone" in member.roles:
@@ -46,14 +50,6 @@ class AgentDoug(commands.Bot):
         if channel is not None:
             await channel.send(
                 "{0} joined the server for the first time at {1}".format(member.display_name, member.joined_at))
-
-    @commands.command()
-    async def help(self, ctx):
-        embedded_help_msg = discord.Embed(colour=discord.Colour.blue(),
-                                          description=self.__help_description)
-        embedded_help_msg.set_author(name=self.__bot_name,
-                                     icon_url=self.__bot_icon_url)
-        await ctx.send("", embed=embedded_help_msg)
 
 
 if __name__ == '__main__':
---
+++ cogs/info.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+
+from agent_doug import AgentDoug
+
+
+class Info:
+    def __init__(self, bot: AgentDoug):
+        self.__bot = bot
+
+    @commands.command(pass_context=True)
+    async def help(self, ctx):
+        embedded_help_msg = discord.Embed(colour=discord.Colour.blue(),
+                                          description=self.__bot.help_description)
+        embedded_help_msg.set_author(name=self.__bot.bot_name,
+                                     icon_url=self.__bot.bot_icon_url)
+        await ctx.send("", embed=embedded_help_msg)
